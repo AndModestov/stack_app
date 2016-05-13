58a4ea8cb3ef188ee327399fa75ebc081703d432
AndModestov <and.modestov@gmail.com>
2016-05-13 11:49:30 +0300

    add reject_if to question/answer models

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,7 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :attachments, dependent: :destroy, as: :attachable
 
-  accepts_nested_attributes_for :attachments, allow_destroy: true
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   validates :title, presence: true, length: { maximum: 90 }
   validates :body, :user_id, presence: true
