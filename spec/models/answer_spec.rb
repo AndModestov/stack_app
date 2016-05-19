94fee0c075eccc35084bb3386e48c4d2a89e60e5
AndModestov <and.modestov@gmail.com>
2016-05-19 14:35:01 +0300

    Add votable_shared_example spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -45,68 +45,11 @@ RSpec.describe Answer, type: :model do
     end
   end
 
-
-  describe 'Votes methods' do
+  it_behaves_like "votable" do
     let(:user){ create(:user) }
     let(:question){ create(:question, user: user) }
     let(:user2){ create(:user) }
-    let!(:answer){ create(:answer, question: question, user: user2) }
-
-    describe 'voted_by?' do
-      it 'should return true if answer is voted by user' do
-        answer.votes.create(user: user, value: 1)
-        expect(answer).to be_voted_by(user)
-      end
-
-      it 'should return false if answer is voted by user' do
-        expect(answer).to_not be_voted_by(user)
-      end
-    end
-
-    describe 'vote_up' do
-      it 'should add + vote to answer' do
-        expect{ answer.vote_up(user) }.to change(answer.votes, :count).by(1)
-        expect(answer.votes.first.value).to be(1)
-      end
-
-      it 'shouldnt add + vote to voted answer' do
-        answer.votes.create(user: user, value: 1)
-
-        expect{ answer.vote_up(user) }.to_not change(answer.votes, :count)
-      end
-    end
-
-    describe 'vote_down' do
-      it 'should add - vote to answer' do
-        expect{ answer.vote_down(user) }.to change(answer.votes, :count).by(1)
-        expect(answer.votes.first.value).to be(-1)
-      end
-
-      it 'shouldnt add + vote to voted answer' do
-        answer.votes.create(user: user, value: -1)
-
-        expect{ answer.vote_down(user) }.to_not change(answer.votes, :count)
-      end
-    end
-
-    describe 'delete_vote' do
-      it 'should delete users vote' do
-        answer.votes.create(user: user, value: 1)
-
-        expect{ answer.delete_vote(user) }.to change(answer.votes, :count).by(-1)
-        expect(Vote.count).to be 0
-      end
-    end
-
-    describe 'total_score' do
-      let!(:user3){ create(:user) }
-
-      it 'should return votable total score' do
-        answer.votes.create(user: user, value: -1)
-        answer.votes.create(user: user3, value: -1)
-
-        expect(answer.total_score).to eq -2
-      end
-    end
+    let!(:votable){ create(:answer, question: question, user: user2) }
   end
+
 end
