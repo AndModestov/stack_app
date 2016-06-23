44323b2103824176423ecd90d937bdc4c2c40b6b
AndModestov <and.modestov@gmail.com>
2016-06-23 10:15:04 +0300

    sphinx schedule configure

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,8 +17,8 @@
    runner "DailyDigestJob.perform_later"
  end
 
- every 60.minutes do
-   rake "ts:index"
+ every 30.minutes do
+   bundle exec rake "ts:index"
  end
 
 # Learn more: http://github.com/javan/whenever
